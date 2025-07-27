cd74bb5ebf356d9d43ba75ab6f2a15073709746b
HassanThePassionate <ali.zinlab@gmail>
2025-07-27 10:36:54 +0500

    Update

--- js/siderbar.js
+++ js/siderbar.js
@@ -1,6 +1,7 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const toggle = document.getElementById('toggle-btn');
+    const sidebarToggle = document.querySelector('.sidebar-btn-toggle')
     sidebar.classList.toggle('show');
     toggle.classList.toggle('move');
 
@@ -10,6 +11,8 @@ function toggleSidebar() {
 </svg>`;
         toggle.style.left = '200px';
         toggle.style.background = 'transparent';
+        sidebarToggle.style.background = 'transparent'
+
     } else {
         toggle.innerHTML = `  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
             <path d="M4 18L20 18" stroke="#ffff" stroke-width="2" stroke-linecap="round" />
@@ -17,6 +20,10 @@ function toggleSidebar() {
             <path d="M4 6L20 6" stroke="#ffff" stroke-width="2" stroke-linecap="round" />
         </svg>`;
         toggle.style.left = '24px';
+        if (window.innerWidth <= 640) {
+
+            sidebarToggle.style.background = 'rgba(0, 0, 0, 0.3)'
+        }
 
     }
 }
